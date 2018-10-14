79c8872b55cec6f94329f3597486fcf9a228484b
daengdaenglee <gunho1020@gmail.com>
2018-10-14 10:25:51 +0900

    05_02_01

---
+++ 05_02_01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int Sum(int value1, int value2) {
+  // int result = value1 + value2;
+  // return result;
+  return value1 + value2;
+}
+
+void main() {
+  // int s;
+  // s = Sum(2, 3);
+  // int s = Sum(2, 3);
+  // printf("Result of Sum function is %d \n", s);
+  printf("Result of Sum function is %d \n", Sum(4, 5));
+}
