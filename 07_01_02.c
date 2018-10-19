940cfedcebba07c460593ef9aa290cb7c3590a3e
daengdaengLee <gunho1020@gmail.com>
2018-10-20 07:24:44 +0900

    07_01_02

---
+++ 07_01_02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+  int data1 = 5, data2 = 3;
+
+  int result1 = data1 + data2;
+  int result2 = data1 * data2;
+  int result3 = data1 / data2;
+  int result4 = data1 % data2;
+
+  printf("result : %d %d %d %d\n", result1, result2, result3, result4);
+}
