781828a9894226942d01cf7833bd6110ef2a39f2
daengdaengLee <gunho1020@gmail.com>
2018-10-20 10:33:08 +0900

    09_02_01

---
+++ 09_02_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+  int sum = 0;
+  int num = 1;
+  while (num <= 5) {
+    printf("num(%d) + sum(%d) = ", num, sum);
+    sum = sum + num;
+    printf("%d\n", sum);
+    num++;
+  }
+  printf("\nResult : num = %d sum = %d\n", num, sum);
+}
