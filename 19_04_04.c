3bdbdf28b62302cc80893c0a70d3d37a2c998345
daengdaengLee <gunho1020@gmail.com>
2018-11-17 22:02:26 +0900

    19_04_04

---
+++ 19_04_04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+  char temp[64];
+  FILE *p_file = fopen("tips.txt", "rt");
+  if (NULL != p_file) {
+    while (EOF != fscanf(p_file, "%s", temp)) {
+      printf("%s\n", temp);
+    }
+    fclose(p_file);
+  }
+}
