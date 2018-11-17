1acaa7af410d634e8c381323ed11f04909b73cc6
daengdaengLee <gunho1020@gmail.com>
2018-11-17 22:19:26 +0900

    19_05_01

---
+++ 19_05_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main() {
+  int data = 0x00000412;
+  FILE *p_file = fopen("jin.dat", "wb");
+  if (NULL != p_file) {
+    fwrite(&data, sizeof(int), 1, p_file);
+    fclose(p_file);
+  }
+}
