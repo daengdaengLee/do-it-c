eceb4a1d54bd68ee3731a2321027bb90d08b398d
daengdaengLee <gunho1020@gmail.com>
2018-11-17 21:59:56 +0900

    19_04_03

---
+++ 19_04_03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+  int num;
+  FILE *p_file = fopen("tipssoft.txt", "rt");
+  if (NULL != p_file) {
+    while (EOF != fscanf(p_file, "%d", &num)) {
+      printf("%d\n", num);
+    }
+    fclose(p_file);
+  }
+}
